9363e9fe99d6163f985739ffda6c0c86234f77c7
BME Preneurs <jearzealots@gmail.com>
2025-02-20 19:27:45 +0530

    modified changes

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -5,11 +5,6 @@ dotenv.config();
 
 export const dbConnect = async () => {
     const mongoURI = process.env.MONGO_URI as string;
-    console.log(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-
     mongoose.connect(mongoURI).then(() => {
         console.log("Connected to the Database");
     }).catch((err) => {
